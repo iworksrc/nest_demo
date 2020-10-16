86e7f10103684594acba26b4a6c0e7568e79e419
rke <imail.for.iwork@gmail.com>
2020-10-16 14:05:03 +0700

    add couple of modules by the nest/cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SiModule } from './modules/si/si.module';
+import { SharedModule } from './modules/shared/shared.module';
 
 @Module({
-  imports: [],
+  imports: [SiModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
